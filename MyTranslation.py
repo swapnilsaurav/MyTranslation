828e6ae965d3abdbab97f4c8fc8d433040f2a19c
swapnilsaurav <swapnil.saurav@gmail.com>
2024-02-12 22:29:22 +0530

    Update MyTranslation.py

--- MyTranslation.py
+++ MyTranslation.py
@@ -4,13 +4,13 @@ import openai
 import prog1
 
 # Set the model engine and your OpenAI API key
-model_engine = "gpt-3.5-turbo-instruct"
+model_engine = "text-davinci-003"
 openai.api_key = prog1.MY_API_KEY
 
 #define a function to handle the translation process
 def translation_process(content,target_lang):
     prompt=f"Translate '{content}' into {target_lang}"
-    response = openai.completions.create(
+    response = openai.Completion.create( 
         model=model_engine,
         prompt=prompt,
         temperature=0.7,
