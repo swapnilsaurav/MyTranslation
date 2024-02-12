257cd253c04f51e5932305ed8c7c23996778f16c
swapnilsaurav <swapnil.saurav@gmail.com>
2024-02-12 22:35:13 +0530

    Update MyTranslation.py

--- MyTranslation.py
+++ MyTranslation.py
@@ -5,7 +5,7 @@ import prog1
 
 # Set the model engine and your OpenAI API key
 model_engine = "text-davinci-003"
-openai.api_key = MY_API_KEY
+openai.api_key = prog1.MY_API_KEY
 
 #define a function to handle the translation process
 def translation_process(content,target_lang):
